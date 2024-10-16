8d8ce89bb8a946b0cd35a9b563f400811f795240
1842047134@qq.com <91363385+mlabalabala@users.noreply.github.com>
2024-10-16 19:06:52 +0800

    Update multi_test.sh

--- live_test/multi_test.sh
+++ live_test/multi_test.sh
@@ -8,6 +8,6 @@ echo 3
 bash livetest.sh 3 > /dev/null 2>&1
 echo 4
 bash livetest.sh 4 > /dev/null 2>&1
-sed -i '1s;^;频道,#genre#\n;' live.txt
 cat ../boxCfg/*.tmp > ../boxCfg/live.txt
+sed -i '1s;^;频道,#genre#\n;' ../boxCfg/live.txt
 rm -f ../boxCfg/*.tmp
